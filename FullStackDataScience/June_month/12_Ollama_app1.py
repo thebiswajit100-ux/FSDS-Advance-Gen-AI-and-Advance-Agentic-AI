585eccd258528eec5af422e91e83b9431617caf0
thebiswajit100-ux <thebiswajit100@gmail.com>
2026-06-15 13:43:54 +0530

    Add files via upload

---
+++ FullStackDataScience/June_month/12_Ollama_app1.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+from ollama import Client
+
+# Create client instance (Very Important)
+client = Client(host="http://localhost:11434") 
+
+
+st.set_page_config(
+    page_title = "Custom LLM model by Prakash Senapati - Ollama",
+    layout= "centered"
+)
+
+st.title("Mr. Prakash Senapati - Ollama App")
+
+prompt = st.text_area("Enter your prompt:", height=200)
+
+if st.button("Generate Response"):
+    if prompt.strip() == "deepseek-r1:1.5b":
+        st.warning("Please enter a prompt.")
+    else:
+        with st.spinner("Thinking..."):
+            response = client.chat(
+                model="deepseek-r1:1.5b",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )    
+            
+            st.success("Response Generated!")
+            st.write(response["message"]["content"])
